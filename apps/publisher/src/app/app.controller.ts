109035ff35f54a8c62a964cc8dfe204e8ee8306b
Giovanni Chiodi <chiodi@unicc.org>
2021-05-25 16:48:09 +0200

    add publisher app with working webhook endpoint

--- apps/publisher/src/app/app.controller.ts
+++ apps/publisher/src/app/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { MeedanCheckClientService } from 'libs/meedan-check-client/src/lib/meedan-check-client.service';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private checkClient: MeedanCheckClientService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post('meedan-reports')
+  publishMeedanReports(@Body() body){
+    const id = body['id'];
+    return this.appService.publishReportById(id);    
   }
 }
--- apps/publisher/src/app/app.module.ts
+++ apps/publisher/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MeedanCheckClientModule} from '@iverify/meedan-check-client';
 
 @Module({
-  imports: [],
+  imports: [MeedanCheckClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
