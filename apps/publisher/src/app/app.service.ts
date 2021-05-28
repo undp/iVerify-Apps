b26a7d2988d86a2307381b8f766b671d6caf5f6a
Giovanni Chiodi <chiodi@unicc.org>
2021-05-28 20:22:06 +0200

    WIP: cleaned publish pipeline

--- apps/publisher/src/app/app.service.ts
+++ apps/publisher/src/app/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { MeedanCheckClientService } from 'libs/meedan-check-client/src/lib/meedan-check-client.service';
+import { CommentStatus, CreatePostDto, PostFormat, PostStatus } from 'libs/wp-client/src/lib/interfaces/create-post.dto';
 import { CreateTagDto } from 'libs/wp-client/src/lib/interfaces/create-tag.dto';
 import { WpClientService } from 'libs/wp-client/src/lib/wp-client.service';
 import { combineLatest, from, iif, Observable, of } from 'rxjs';
@@ -14,22 +15,41 @@ export class AppService {
   
   publishReportById(id: string): Observable<any>{
     const report$: Observable<any> = this.checkClient.getReport(id);
-    const reportTags$: Observable<any> = report$.pipe(
-      map(report => report.tags)
-    )
-    const tagsIds$: Observable<number[]> = reportTags$.pipe(
+    const tagsIds$: Observable<number[]> = report$.pipe(
+      map(report => report.tags),
       switchMap(tags => iif(() => !!tags.length, this.tagsIds(tags), of(null)))
     )
-
-    //TODO: author, gategories
-
-    return combineLatest([report$, tagsIds$]).pipe(
-      map(([report, tags]) => this.buildPostFromReport(report, tagsIds)),
+    const post$: Observable<any> = combineLatest([report$, tagsIds$]).pipe(
+      map(([report, tagsIds]) => this.buildPostFromReport(report, tagsIds)),
       switchMap(postDto => this.wpClient.publishPost(postDto))
     )
+
+    //TODO: 
+    // - manage categories similarly to tags
+    // - after the report is created we must create the associated media
+    // - get the posts author 
+
+    return report$
   }
 
-  tagsIds(tags: string[]): Observable<number[]>{
+  private buildPostFromReport(report: any, tags: number[]): CreatePostDto{
+    const status = PostStatus.publish;
+    const comment_status = CommentStatus.open;
+    const format = PostFormat.standard;
+    return {
+      format,
+      //author,
+      // title,
+      // content,
+      // meta,
+      comment_status,
+      status,
+      // categories,
+      tags
+    }
+  }
+
+  private tagsIds(tags: string[]): Observable<number[]>{
     const wpTags$: Observable<any> = this.wpClient.listTags();
     const existingTagsIds$: Observable<number[]> = wpTags$.pipe(
       map(wpTags => wpTags.filter(tag => tags.indexOf(tag.name) > -1).map(tag => tag.id))
@@ -49,14 +69,14 @@ export class AppService {
     return tagsIds$;
   }
 
-  createManyTags(tags: string[]): Observable<any>{
+  private createManyTags(tags: string[]): Observable<any>{
     const tagsDtos: CreateTagDto[] = tags.map(tag => ({name: tag})) 
     return from(tagsDtos).pipe(
       concatMap(tag => this.createSingleTag(tag))
     )
   }
 
-  createSingleTag(tag: CreateTagDto): Observable<any>{
+  private createSingleTag(tag: CreateTagDto): Observable<any>{
     return this.wpClient.createTag(tag)
   }
 }
