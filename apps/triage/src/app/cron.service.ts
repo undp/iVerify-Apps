eec9bc6fd0454554e7d6ad4c8c9bafdcff6f8005
Giovanni Chiodi <chiodi@unicc.org>
2021-06-22 16:05:17 +0200

    changed cron job period

--- apps/triage/src/app/cron.service.ts
+++ apps/triage/src/app/cron.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { HttpException, Injectable, Logger } from "@nestjs/common";
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { AppService } from "./app.service";
 
@@ -8,17 +8,18 @@ export class CronService{
 
     constructor(private appService: AppService){}
     
-    @Cron(CronExpression.EVERY_10_MINUTES)
+    @Cron(CronExpression.EVERY_HOUR)
     async analyze(){
         try{
             const endDate = new Date().toISOString();
             const start = new Date();
-            start.setHours(new Date().getHours() -6);
+            start.setHours(new Date().getHours() -1);
             const startDate = start.toISOString();
             this.logger.log(`Running cron job with startDate ${startDate} and endDate ${endDate}`)
             return await this.appService.analyze(startDate, endDate);
         } catch(e){
             this.logger.error('Cron job error: ', e.message);
+            throw new HttpException(e.message, 500);
         }
     }
 }
