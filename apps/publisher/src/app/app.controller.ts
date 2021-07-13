25c4678b97efa3f903434cadb8afc4d803d0780d
Giovanni Chiodi <chiodi@unicc.org>
2021-07-13 11:07:55 +0200

    add more logs

--- apps/publisher/src/app/app.controller.ts
+++ apps/publisher/src/app/app.controller.ts
@@ -30,6 +30,8 @@ export class AppController {
       const event = parsed.event;
       const data = parsed.data;
       Object.keys(data).forEach(key => this.logger.log('data key', key))
+      const project_media = data.project_media;
+      Object.keys(project_media).forEach(key => this.logger.log('project_media key', key))
       this.logger.log('received event: ', event);
       if(event === 'update_project_media'){
         const id = parsed.data.project_media.dbid;
