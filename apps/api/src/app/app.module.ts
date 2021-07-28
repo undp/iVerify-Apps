4773186412d2eae8fcd01da8d5dd1c22b58b8355
Giovanni Chiodi <chiodi@unicc.org>
2021-07-28 10:33:27 +0200

    added env variables

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -32,11 +32,11 @@ import { StatsCronService } from './stats/cron.service';
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'undphq10ds002.mysql.database.azure.com',
-      port: 3306,
-      username: 'iverify@undphq10ds002',
-      password: 'KKZ1pTyNiRbL',
-      database: 'iverify',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       insecureAuth: true,
       autoLoadEntities: true,
       synchronize: true
