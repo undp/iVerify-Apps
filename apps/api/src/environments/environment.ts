c24d74d6f989c439b07b8105f65ec441562170e2
Pramod <fulpagare@unicc.org>
2021-11-03 18:02:14 +0530

    changes to fix

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
   jWTSecretRefreshToken: 'UmavWLNIMKfsF359Uz8dlLWZkwMeGsLM',
   clientID: 'BXh9T50VmIHw9CPycvVAziPOtIUgFPJaqgS2o0jV',
   clientSecret: '0uNZb7tEqcxueUe1D3VhmCDmMVwGBNhKV97BnZXY',
-  redirectUri: 'http://localhost:4300/',
+  redirectUri: 'http://20.93.245.255/',
   wpPassword: 'RHc6RJuW*f4FMmOPKao(q(Hh'
 };
