22c2161de44031a752f625e2806b50f54b776ea4
joniwan <chiodi@unicc.org>
2021-12-03 14:18:46 +0100

    fixed apiPublisherHelper export

--- apps/publisher/src/api-publisher/api-publisher.module.ts
+++ apps/publisher/src/api-publisher/api-publisher.module.ts
@@ -2,10 +2,11 @@ import { ApiClientModule } from '@iverify/api-client';
 import { HttpModule, Module } from '@nestjs/common';
 import { SharedModule } from '../shared/shared.module';
 import { ApiPublisherService } from './api-publisher.service';
+import { ApiPublisherHelper } from './helper';
 
 @Module({
   imports: [HttpModule, SharedModule, ApiClientModule],
-  providers: [ApiPublisherService],
+  providers: [ApiPublisherService, ApiPublisherHelper],
   exports: [ApiPublisherService]
 })
 export class ApiPublisherModule {}
