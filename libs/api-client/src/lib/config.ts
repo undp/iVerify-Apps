f80b2e59d2e3562823a5e6a7ad2cd19153e10fd1
joniwan <chiodi@unicc.org>
2021-12-06 18:12:25 +0100

    fixed post article

--- libs/api-client/src/lib/config.ts
+++ libs/api-client/src/lib/config.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class ApiClientConfig{
     readonly apiUrl = process.env.API_URL;
-    readonly postArticleUrl = this.apiUrl + 'articles/save-article';
+    readonly postArticleUrl = this.apiUrl + '/articles/save-article';
 
 }
