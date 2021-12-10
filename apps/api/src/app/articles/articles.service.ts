7092e41f386d82581143600afc28bc1c709aad11
joniwan <chiodi@unicc.org>
2021-12-10 11:58:00 +0100

    debug save article

--- apps/api/src/app/articles/articles.service.ts
+++ apps/api/src/app/articles/articles.service.ts
@@ -23,7 +23,7 @@ export class ArticlesService{
     }
 
     async saveOne(article: Partial<Article>){
-        this.logger.log(`Saving article ${article}`)
+        this.logger.log(`Saving article ${JSON.stringify(article)}`)
         const newRecord = await this.articleRepository.create(article);
         return this.articleRepository.save(newRecord);
     }
