fde02c756705b7a0e1210113f73db2b62c62a7d1
joniwan <chiodi@unicc.org>
2021-12-10 16:29:44 +0100

    add articles by range

--- libs/email/src/lib/email.service.ts
+++ libs/email/src/lib/email.service.ts
@@ -7,18 +7,51 @@ let converter = require('json-2-csv');
 export class EmailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendCsvReport(data: Article[]) {
+  async sendCsvReport(data: Article[], date: string) {
 
     // const articles = JSON.stringify(data);
+    // const keys = [
+    // 'DB Id',
+    // 'Meedan Id',
+    // 'WP Id',
+    // 'Title',
+    // 'Content',
+    // 'WP Url',
+    // 'Publication Date',
+    // 'Tags',
+    // 'Threat Level',
+    // 'type of Violation',
+    // 'Claim',
+    // 'Rating Justification',
+    // 'Evidence and References',
+    // 'Mis/Disinfomration Type',
+    // 'Hate Speech Type',
+    // 'Toxic Score',
+    // 'Obscene Score',
+    // 'Attack on Identity Score',
+    // 'Threat Score',
+    // 'Sexually Explicit Score',
+    // 'dToxicScore',
+    // 'dObsceneScore',
+    // 'dIdentityScore',
+    // 'dThreatScore',
+    // 'dExplicitScore',
+    // 'dInsultScore',
+    // 'Main Source Name',
+    // 'Main Source Url',
+    // 'Notes'
+    // ]
     const csv = await converter.json2csvAsync(data);
+    const msg = `
+    `
     await this.mailerService.sendMail({
-      to: 'chiodigiovanni1@gmail.com',
+      to: 'chiodigiovanni1@gmail.com, benet@unicc.org',
       from: 'no_reply@un-icc.cloud',
-      subject: 'iVerify publications report',
+      subject: 'Daily iVerify publications - Honduras-test',
       text: 'CSV report',
       attachments: [
         {   
-            filename: 'csv.txt',
+            filename: `iverify-publications-${date}.csv`,
             content: csv
         },
         ]
