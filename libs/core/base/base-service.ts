b3070128814f6d583505619e70ecee12dfcf34c9
Luis Teixeira <luis.teixeira@gmail.com>
2023-01-16 09:34:54 +0000

    added location header in any api call

--- libs/core/base/base-service.ts
+++ libs/core/base/base-service.ts
@@ -1,50 +1,73 @@
-import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import {
+    HttpClient,
+    HttpParams,
+    HttpErrorResponse,
+    HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { StorageService } from '../storage/storage.service';
-import { of, Observable } from 'rxjs';
+import { of, Observable, Subscription } from 'rxjs';
+import { selectLocation } from '@iverify/core/store/selectors/locations.selector';
+import { Location as LocationData } from '@iverify/core/models/location';
+import { AppState } from '../store/states/app.state';
+import { Store } from '@ngrx/store';
 
 const APP_STATE_KEY = environment.app_state_key;
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class BaseService {
-  constructor(public http?: HttpClient, public storageService?: StorageService) {}
+    location$: Observable<LocationData>;
+    locationId: string;
+    subLocation: Subscription;
+    headers: HttpHeaders | { [header: string]: string | string[] };
 
-  readonly uris = {};
+    constructor(
+        public http?: HttpClient,
+        public storageService?: StorageService,
+        private store?: Store<AppState>
+    ) {
+        this.location$ = this.store.select(selectLocation);
 
-  getUrl(uri: string, withAPIVersion: boolean = false) {
-    return `${environment.api.base}/${
-      withAPIVersion ? `api/${environment.api.version}/` : ''
-    }${uri}`;
-  }
-
-  getParamsFromObject(object: any): HttpParams {
-    return object
-      ? Object.keys(object)
-          .map(key => ({ key, value: object[key] }))
-          .reduce((params, { key, value }) => {
-            return params.set(key, value);
-          }, new HttpParams())
-      : null;
-  }
-
-  readAsBlob(url: string): Observable<Blob> {
-    return this.http.get(url, {
-      responseType: 'blob'
-    });
-  }
-
-  cacheHandler(err: Error, key: string) {
-    if (err instanceof HttpErrorResponse) {
-      if (err.status === 0  && err.statusText == 'Unknown Error') {
-        let appData = this.storageService.get(APP_STATE_KEY);
-        return of((appData[key])? appData[key][key]: null);
-      }
+        this.subLocation = this.location$.subscribe((locationId) => {
+            this.locationId = locationId.id;
+            this.headers = { location: locationId.id };
+        });
     }
-    return null;
-  }
+
+    readonly uris = {};
+
+    getUrl(uri: string, withAPIVersion: boolean = false) {
+        return `${environment.api.base}/${
+            withAPIVersion ? `api/${environment.api.version}/` : ''
+        }${uri}`;
+    }
+
+    getParamsFromObject(object: any): HttpParams {
+        return object
+            ? Object.keys(object)
+                  .map((key) => ({ key, value: object[key] }))
+                  .reduce((params, { key, value }) => {
+                      return params.set(key, value);
+                  }, new HttpParams())
+            : null;
+    }
+
+    readAsBlob(url: string): Observable<Blob> {
+        return this.http.get(url, {
+            responseType: 'blob',
+        });
+    }
+
+    cacheHandler(err: Error, key: string) {
+        if (err instanceof HttpErrorResponse) {
+            if (err.status === 0 && err.statusText == 'Unknown Error') {
+                let appData = this.storageService.get(APP_STATE_KEY);
+                return of(appData[key] ? appData[key][key] : null);
+            }
+        }
+        return null;
+    }
 }
-
-
