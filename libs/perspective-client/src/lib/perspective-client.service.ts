a5a4f5e455af92503307b56465a5ed98b1444e45
palindaa <palinda@xeptagon.com>
2023-11-28 14:11:40 +0530

    perspective API language

--- libs/perspective-client/src/lib/perspective-client.service.ts
+++ libs/perspective-client/src/lib/perspective-client.service.ts
@@ -34,6 +34,9 @@ export class PerspectiveClientService{
                     PROFANITY: {scoreThreshold: threshold}
                 } 
            }
+        if (process.env.language != 'en') {
+            body.languages.push(process.env.language)
+        } 
         return body;
     }
 
