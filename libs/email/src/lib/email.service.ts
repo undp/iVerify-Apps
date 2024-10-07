09fa0cdad6e014ad88805fa4cb2b95a426ef87ce
Janith Rathnayaka <janithr@xeptaon.com>
2024-10-07 17:28:40 +0530

    update html for text email content

--- libs/email/src/lib/email.service.ts
+++ libs/email/src/lib/email.service.ts
@@ -64,21 +64,18 @@ export class EmailService {
       await this.mailerService.sendMail({
         to: email,
         subject: 'NOTIFICATION EMAIL FOR SUBMITTED FACT-CHECKED CONTENT',
-        html: `
-        <html>
-          <body style="font-family: Arial, sans-serif; color: #333;">
-            <div style="background-color: #f8f8f8; padding: 20px; border-radius: 10px;">
-              <h2 style="color: #0066cc;">Bonjour</h2>
-              <p>Vous avez récemment souscrit à notre service de notifications pour être informé lorsque du contenu est vérifié par l’equipe iVerify.</p>
-              <p>Nous vous informons que le contenu que vous aviez signalé ou suivi a été fact-checké. Vous pouvez consulter les résultats de notre analyse en suivant le lien ci-dessous :</p>
-              <p><a href="${factCheckedLink}" style="color: #0066cc; text-decoration: none;">${factCheckedLink}</a></p>
-              <p>Nous vous remercions pour votre engagement envers la vérification des informations et vous encourageons à partager ces résultats avec vos contacts sur les réseaux sociaux afin de contribuer à la diffusion d'informations fiables.</p>
-              <br>
-              <p>Cordialement,<br>L’équipe de vérification des faits iVerify</p>
-            </div>
-          </body>
-        </html>
-      `
+        text: `Bonjour,
+
+        Vous avez récemment souscrit à notre service de notifications pour être informé lorsque du contenu est vérifié par l’equipe iVerify.
+
+        Nous vous informons que le contenu que vous aviez signalé ou suivi a été fact-checké. Vous pouvez consulter les résultats de notre analyse en suivant le lien ci-dessous :
+
+        ${factCheckedLink}
+
+        Nous vous remercions pour votre engagement envers la vérification des informations et vous encourageons à partager ces résultats avec vos contacts sur les réseaux sociaux afin de contribuer à la diffusion d'informations fiables.
+
+        Cordialement,
+        L’équipe de vérification des faits iVerify`,
     });
       console.log('Email sent successfully');
     } catch (error) {
