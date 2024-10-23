4d3c7b681a8f49f6ff83efe9e6914a212378afb3
Janith Rathnayaka <janithr@xeptaon.com>
2024-10-23 22:22:46 +0530

    add comment for get posts Date function

--- libs/wp-client/src/lib/wp-client.service.ts
+++ libs/wp-client/src/lib/wp-client.service.ts
@@ -59,6 +59,7 @@ export class WpClientService{
       const start = new Date();
       start.setHours(start.getHours() - 24);
       const startDate = date ?? start.toISOString().split('.')[0];
+      console.log('getPostsFromDate',startDate)
       return this.http.get(this.config.endpoints.posts + '?after=' + startDate  + '&per_page=100' , this.auth).pipe(
           map(res => res.data),
           catchError(err => {
