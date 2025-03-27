b3995d1d00ae9e236419443c92dcef693061d13d
Kalindhu Navanjana <kalindud@xeptagon.com>
2025-03-27 12:52:47 +0530

    Fixed the post Duplication Issue.

--- apps/publisher/src/shared/shared.service.ts
+++ apps/publisher/src/shared/shared.service.ts
@@ -1,37 +1,72 @@
-import { Injectable, Scope } from "@nestjs/common";
-import { MeedanCheckClientService } from "libs/meedan-check-client/src/lib/meedan-check-client.service";
-import { Observable, Subject } from "rxjs";
-import { shareReplay, switchMap, take } from "rxjs/operators";
+import { Injectable, Logger, Scope } from '@nestjs/common';
+import { MeedanCheckClientService } from 'libs/meedan-check-client/src/lib/meedan-check-client.service';
+import { Observable, Subject } from 'rxjs';
+import { shareReplay, switchMap, take, tap } from 'rxjs/operators';
 
 @Injectable({ scope: Scope.REQUEST })
-export class SharedService{
-    private _reportId: Subject<string> = new Subject<string>();
-    reportId$: Observable<string> = this._reportId.asObservable().pipe(take(1), shareReplay(1));
+export class SharedService {
+  constructor(private checkClient: MeedanCheckClientService) {}
+  private _reportId: Subject<string> = new Subject<string>();
+  private _report = new Subject<any>();
+  private _meedanReport = new Subject<any>();
+  private logger = new Logger("PublisherSharedService");
 
-    private _wpPost: Subject<any> = new Subject<any>();
-    wpPost$: Observable<string> = this._wpPost.asObservable().pipe(take(1), shareReplay(1));
+  reportId$: Observable<string> = this._reportId.asObservable().pipe(
+    // take(1),
+    tap((value) => this.logger.log(`Report ID emitted: ${value}`)),
+    shareReplay(1)
+  );
 
-    report$: Observable<any> = this.reportId$.pipe(
-        switchMap(id => this.checkClient.getReport(id)),   
-        shareReplay(1)     
-        )
-    meedanReport$: Observable<any> = this.reportId$.pipe(
-        switchMap(id => this.checkClient.getMeedanReport(id)),   
-        shareReplay(1)     
-        )
-    private sub = this.report$.subscribe();
-    private wpPostSub = this.wpPost$.subscribe();
+  report$: Observable<any> = this._report
+    .asObservable()
+    .pipe(
+      tap((report) => this.logger.log(`Report emitted:${report}`))
+    );
 
-    
-    constructor(private checkClient: MeedanCheckClientService){}
-    
-    updateReportId(id: string){
-        this._reportId.next(id);
-    }
+  meedanReport$: Observable<any> = this._meedanReport
+    .asObservable()
+    .pipe(
+      tap((meedanReport) =>
+        this.logger.log(`Meedan Report Emitted: ${meedanReport}`)
+      )
+    );
 
-    updateWpPost(wpPost: any){
-        this._wpPost.next(wpPost);
-    }
+  private _wpPost: Subject<any> = new Subject<any>();
+  wpPost$: Observable<string> = this._wpPost
+    .asObservable()
+    .pipe(take(1), shareReplay(1));
 
+  private fetchReportObserver = this.reportId$
+    .pipe(
+      tap((id) => this.logger.log(`Report initialized with id: ${JSON.stringify(id)}`)),
+      tap((id) => this.logger.log('Report Fetching...')),
+      switchMap((id) => this.checkClient.getReport(id)),
+      tap((report) => this._report.next(report))
+      // shareReplay(1)
+    )
+    .subscribe();
 
+  private meedanReportObserver = this.reportId$
+    .pipe(
+      tap((id) => this.logger.log(`Meedan Report initialized with id: ${JSON.stringify(id)}`)),
+      tap((id) => this.logger.log('Meedan Report Fetching...')),
+      switchMap((id) => this.checkClient.getMeedanReport(id)),
+      tap((meedanReport) => this._meedanReport.next(meedanReport))
+      // shareReplay(1)
+    )
+    .subscribe();
+
+  private rID = this.reportId$.subscribe({
+    next: (value) => {
+        this.logger.log(value);
+    },
+  });
+
+  updateReportId(id: string) {
+    this._reportId.next(id);
+  }
+
+  updateWpPost(wpPost: any) {
+    this._wpPost.next(wpPost);
+  }
 }
